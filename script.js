8a17a048c35d1a945df3d9d2ddf4d6774f36636b
Swanand Kulkarni <swanandkulkarni@benchmarkit.solutions>
2021-08-18 15:51:58 +0530

    Javascript Assignment Start

---
+++ script.js
@@ -0,0 +1,87 @@
+/*
+// Coding Challenge : 1
+const markHeight = 1.88;
+const johnHeight = 1.76;
+const markWeight = 95;
+const johnWeight = 85;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+
+// Coding Challenge : 2
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI}) BMI`);
+} else if (markBMI < johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is lower than John's (${johnBMI}) BMI`);
+} else {
+  console.log(`Mark's BMI (${markBMI}) and John's (${johnBMI} BMI are equal)`);
+}
+*/
+
+// Coding Challenge : 3
+let dolphinMatch1 = 96;
+let dolphinMatch2 = 108;
+let dolphinMatch3 = 89;
+let koalasMatch1 = 88;
+let koalasMatch2 = 91;
+let koalasMatch3 = 108;
+const averageDolphins = (dolphinMatch1 + dolphinMatch2 + dolphinMatch3) / 3;
+const averageKoalas = (koalasMatch1 + koalasMatch2 + koalasMatch3) / 3;
+
+/*
+// Values, Variables, let, const and var, basic operators
+const country = "India";
+const continent = "Asia";
+let population = 110;
+console.log(country, continent, population + " million");
+
+const isIsland = false;
+let language;
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
+
+language = "Hindi";
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
+
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+population++;
+console.log(population);
+
+let populationFinland = 6;
+const moreThanFinland = population > populationFinland;
+console.log(moreThanFinland);
+
+const averagePopulation = 33;
+
+const lessPopulation = population < averagePopulation;
+console.log(lessPopulation);
+
+// Template Literals
+const description = `${country} is in ${continent} and its ${population} million people speak ${language}`;
+console.log(description);
+
+// if-else Statements
+if (population > averagePopulation) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${averagePopulation - population} below average`
+  );
+}
+*/
